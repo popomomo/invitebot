779a9e31df8abcaff64c78d84e09944e5ee22159
pOpO <winit.g@gmail.com>
2021-09-14 14:06:11 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,7 +114,7 @@ async def status_task():
         except:
             pass
         
-WHEN = time(10, 0, 0)  # 6:00 PM
+WHEN = datetime.time(10, 0, 0)  # 6:00 PM
 channel_id = 829651715502374982 # Put your channel id here
 
 async def called_once_a_day():  # Fired every day
@@ -123,18 +123,18 @@ async def called_once_a_day():  # Fired every day
     await channel.send(";fg")
 
 async def background_task():
-    now = datetime.utcnow()
+    now = datetime.datetime.utcnow()
     if now.time() > WHEN:  # Make sure loop doesn't start after {WHEN} as then it will send immediately the first time as negative seconds will make the sleep yield instantly
-        tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+        tomorrow = datetime.datetime.combine(now.date() + timedelta(days=1), time(0))
         seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
         await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start 
     while True:
-        now = datetime.utcnow() # You can do now() or a specific timezone if that matters, but I'll leave it with utcnow
-        target_time = datetime.combine(now.date(), WHEN)  # 6:00 PM today (In UTC)
+        now = datetime.datetime.utcnow() # You can do now() or a specific timezone if that matters, but I'll leave it with utcnow
+        target_time = datetime.datetime.combine(now.date(), WHEN)  # 6:00 PM today (In UTC)
         seconds_until_target = (target_time - now).total_seconds()
         await asyncio.sleep(seconds_until_target)  # Sleep until we hit the target time
         await called_once_a_day()  # Call the helper function that sends the message
-        tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+        tomorrow = datetime.datetime.combine(now.date() + timedelta(days=1), time(0))
         seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
         await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start a new iteration
 
